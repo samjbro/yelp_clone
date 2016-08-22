07e897b2b85dd9fe4c8a86686d5f8a57e3a40f7b
Harry Jaggs <harryjaggs92@gmail.com>
2016-08-22 15:49:53 +0100

    corrected typos

--- spec/features/restaurant_feature_spec.rb
+++ spec/features/restaurant_feature_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'restaurants' do
-  content 'no restaurants have been added' do
+  context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
