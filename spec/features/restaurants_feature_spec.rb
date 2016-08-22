966323cc83a411945b55cdfdc9af42f55112c898
Sara <sarah.alalaya@gmail.com>
2016-08-22 22:11:36 +0400

    added view for a restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,11 +1,27 @@
 require 'rails_helper'
 
 feature 'restaurants' do
+
   context 'no restaurants have been added' do
     scenario 'it should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content "No restaurants yet"
       expect(page).to have_link "Add a restaurant"
     end
+  end
+
+
+  context 'restaurants have been added' do
+
+    before do
+      Restaurant.create(name: 'Salad Boutique')
+    end
+
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('Salad Boutique')
+      expect(page).not_to have_content "No restaurants yet"
+    end
+    
   end
 end
