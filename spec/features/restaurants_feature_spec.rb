6d112022fd54a9c5e822de19b395d7e4938d8610
Sara <sarah.alalaya@gmail.com>
2016-08-23 12:59:14 +0400

    Challenge 8 completed

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -22,6 +22,16 @@ feature 'restaurants' do
       expect(page).to have_content('Salad Boutique')
       expect(page).not_to have_content "No restaurants yet"
     end
-    
+  end
+
+  context 'creating restaurants' do
+    scenario 'promots user to fill out a form, then displays the new restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'Salad Boutique'
+      click_button 'Create Restaurant'
+      expect(current_path).to eq '/restaurants'
+      expect(page).to have_content('Salad Boutique')
+    end
   end
 end
