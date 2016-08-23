cf18148a57a19cc033d17f71e57395b9fde2ec7a
benjamin-white <benjaminwhite@live.co.uk>
2016-08-23 16:50:51 +0100

    adds validations for name uniqueness and rating inclusion

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,10 +2,16 @@ require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
 
-it 'is not valid with a name less than a 3 characters' do
-  restaurant = Restaurant.new(name: 'KF')
-  expect(restaurant).to have(1).error_on(:name)
-  expect(restaurant).not_to be_valid
-end
+  it 'is not valid with a name less than a 3 characters' do
+    restaurant = Restaurant.new(name: 'KF')
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
+
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 
 end
