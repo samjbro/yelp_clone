dd57c54f0438f7c7aac86e37b22914659fc898af
Sara <sarah.alalaya@gmail.com>
2016-08-23 15:10:40 +0400

    Challenge 11 completed

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -61,4 +61,15 @@ feature 'restaurants' do
       expect(current_path).to eq '/restaurants'
     end
   end
+
+  context 'deleting restaurants' do
+    before { Restaurant.create(name: 'KFC', description: 'Deep fried goodness') }
+
+    scenario 'scenario description' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
 end
