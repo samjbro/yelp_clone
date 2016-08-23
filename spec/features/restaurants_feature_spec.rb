e3de907e96982ff19da7e07899e72808fc5222fd
6eff <6eff@mail.ru>
2016-08-23 15:18:19 +0300

    destroys restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -9,55 +9,62 @@ feature 'restaurants' do
     end
   end
 
+  context 'restaurants have been added' do
+    before do
+      Restaurant.create(name: 'KFC')
+    end
 
-context 'restaurants have been added' do
-  before do
-    Restaurant.create(name: 'KFC')
+    scenario 'display restaurants' do
+      visit '/restaurants'
+      expect(page).to have_content('KFC')
+      expect(page).not_to have_content('No restaurants yet')
+    end
   end
 
-  scenario 'display restaurants' do
-    visit '/restaurants'
-    expect(page).to have_content('KFC')
-    expect(page).not_to have_content('No restaurants yet')
-  end
-end
-
-context 'creating restaurants' do
-  scenario 'prompt users to fill out a form, and then display the results' do
-    visit '/restaurants'
-    click_link 'Add a restaurant'
-    fill_in 'Name', with: 'KFC'
-    click_button 'Create Restaurant'
-    expect(page).to have_content 'KFC'
-    expect(current_path).to eq '/restaurants'
-  end
-end
-
-context 'view restaurants' do
-  let!(:kfc) { Restaurant.create(name: 'KFC') }
-  scenario 'lets a user view a restaurant' do
-    visit '/restaurants'
-    click_link 'KFC'
-    expect(page).to have_content 'KFC'
-    expect(current_path).to eq "/restaurants/#{kfc.id}"
-  end
-end
-
-context 'editing restaurants' do
-
-  before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
-
-  scenario 'let a user edit a restaurant' do
-    visit '/restaurants'
-    click_link 'Edit KFC'
-    fill_in 'Name', with: 'Kentucky Fried Chicken'
-    fill_in 'Description', with: 'Deep fried goodness'
-    click_button 'Update Restaurant'
-    expect(page).to have_content 'Kentucky Fried Chicken'
-    expect(page).to have_content 'Deep fried goodness'
-    expect(current_path).to eq '/restaurants'
+  context 'creating restaurants' do
+    scenario 'prompt users to fill out a form, and then display the results' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
+    end
   end
 
-end
+  context 'view restaurants' do
+    let!(:kfc) { Restaurant.create(name: 'KFC') }
+    scenario 'lets a user view a restaurant' do
+      visit '/restaurants'
+      click_link 'KFC'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq "/restaurants/#{kfc.id}"
+    end
+  end
+
+  context 'editing restaurants' do
+      before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
+
+      scenario 'lets a user edit a restaurant' do
+        visit '/restaurants'
+        click_link 'Edit KFC'
+        fill_in 'Name', with: 'Kentucky Fried Chicken'
+        fill_in 'Description', with: 'Deep fried goodness'
+        click_button 'Update Restaurant'
+        expect(page).to have_content 'Kentucky Fried Chicken'
+        expect(page).to have_content 'Deep fried goodness'
+        expect(current_path).to eq '/restaurants'
+      end
+  end
+
+  context 'deleting restaurants' do
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
+    scenario 'removes a restaurant when user clicks delete link' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
 
 end
