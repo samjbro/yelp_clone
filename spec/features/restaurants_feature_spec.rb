e68b2b3df9d7f94fa36b707d92fa6474ec1b8cf9
6eff <6eff@mail.ru>
2016-08-23 13:58:44 +0300

    viewing restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-
 feature 'restaurants' do
   context 'when there are no restaurants' do
     scenario 'should have a prompt to add a restaurant' do
@@ -21,15 +20,25 @@ context 'restaurants have been added' do
     expect(page).to have_content('KFC')
     expect(page).not_to have_content('No restaurants yet')
   end
+end
 
-  context 'creating restaurants' do
-    scenario 'prompt users to fill out a form, and then display the results' do
-      visit '/restaurants'
-      click_link 'Add a restaurant'
-      fill_in 'Name', with: 'KFC'
-      click_button 'Create Restaurant'
-      expect(page).to have_content 'KFC'
-      expect(current_path).to eq '/restaurants'
-    end
+context 'creating restaurants' do
+  scenario 'prompt users to fill out a form, and then display the results' do
+    visit '/restaurants'
+    click_link 'Add a restaurant'
+    fill_in 'Name', with: 'KFC'
+    click_button 'Create Restaurant'
+    expect(page).to have_content 'KFC'
+    expect(current_path).to eq '/restaurants'
+  end
+end
+
+context 'view restaurants' do
+  let!(:kfc) { Restaurant.create(name: 'KFC') }
+  scenario 'lets a user view a restaurant' do
+    visit '/restaurants'
+    click_link 'KFC'
+    expect(page).to have_content 'KFC'
+    expect(current_path).to eq "/restaurants/#{kfc.id}"
   end
 end
