57c19b6f597f61f880edb09f8a5415db6435a123
Nomi Vos <nvoswork@gmail.com>
2016-08-23 04:55:45 -0500

    finishes step 7

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature 'restaurants' do
+  
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
