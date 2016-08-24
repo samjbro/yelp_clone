f3e8eab8fb3b87db9ab27d74773113714e3f1093
samjbro <sjbroughton1@gmail.com>
2016-08-24 21:49:19 +0100

    completes v3 of challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
   has_many :reviews,
-          -> {extending WithUserAssociationExtension },
+          -> { extending WithUserAssociationExtension },
           dependent: :destroy
-          
+
   validates :name, length: { minimum: 3}, uniqueness: true
 end
