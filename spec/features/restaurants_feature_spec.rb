e54a251f64b8c1c436b919e6175c58ee4b31d4f5
Richard Bates <richo225@gmail.com>
2016-08-24 13:25:42 +0100

    User limits. Must be logged in to create restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -23,17 +23,29 @@ feature 'restaurants' do
 
   context 'creating restaurants' do
     scenario 'prompt users to fill out a form, and then display the results' do
-      visit '/restaurants'
-      click_link 'Add a restaurant'
-      fill_in 'Name', with: 'KFC'
+      sign_up
+      create_restaurant
       click_button 'Create Restaurant'
       expect(page).to have_content 'KFC'
       expect(current_path).to eq '/restaurants'
     end
 
+    scenario "user must be signed in to create a restaurant" do
+      visit "/"
+      click_link "Add a restaurant"
+      expect(page).to have_content "You need to sign in or sign up before continuing."
+    end
+
+    scenario "signed in user creating a restaurant increases count by 1" do
+      sign_up
+      create_restaurant
+      expect{click_button 'Create Restaurant'}.to change(Restaurant, :count).by(1)
+    end
+
     context 'an invalid restaurant' do
       it 'does not let you submit a name that is too short' do
-        visit '/restaurants'
+        sign_up
+        visit '/'
         click_link 'Add a restaurant'
         fill_in 'Name', with: 'KF'
         click_button 'Create Restaurant'
@@ -57,6 +69,7 @@ feature 'restaurants' do
       before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
 
       scenario 'lets a user edit a restaurant' do
+        sign_up
         visit '/restaurants'
         click_link 'Edit KFC'
         fill_in 'Name', with: 'Kentucky Fried Chicken'
@@ -71,6 +84,7 @@ feature 'restaurants' do
   context 'deleting restaurants' do
     before { Restaurant.create name: 'KFC', description: 'Deep fried goodness' }
     scenario 'removes a restaurant when user clicks delete link' do
+      sign_up
       visit '/restaurants'
       click_link 'Delete KFC'
       expect(page).not_to have_content 'KFC'
